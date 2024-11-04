6f51dd8ff1f6082b37138fa34dab94bf5504cff3
Richard <richardzou25@gmail.com>
2024-11-04 09:35:56 -0500

    Created two functions to start - one constructor function for Book objects - one function to add books to library array

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
+// Test Book
+const billySummers = new Book('Billy Summers', 'Stephen King');
+console.log(billySummers);
+
+function addBookToLibrary(title, author) {
+
+}
