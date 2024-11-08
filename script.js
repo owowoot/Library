f83829de008710e7935d29c55fd73d7b36082f14
Richard <richardzou25@gmail.com>
2024-11-08 16:44:06 -0500

    added readStatus as a property

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const myLibrary = [
-    { title: "Billy Summers", author: "Stephen King" },
-    { title: "It", author: "Stephen King" },
-    { title: "Atomic Habits", author: "James Clear" }
+    { title: "Billy Summers", author: "Stephen King", readStatus: true },
+    { title: "It", author: "Stephen King", readStatus: false },
+    { title: "Atomic Habits", author: "James Clear", readStatus: true },
+    { title: "Bonechiller", author: "Graham McNamee", readStatus: true }
 ];
 
 class Book {
-    constructor(title, author) {
+    constructor(title, author, readStatus) {
         this.title = title;
         this.author = author;
+        this.readStatus = readStatus;
     }
 }
 
-// Test Book
-const billySummers = new Book('Billy Summers', 'Stephen King');
-console.log(billySummers);
+// // Test Book
+// const billySummers = new Book('Billy Summers', 'Stephen King');
+// console.log(billySummers);
 
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author) {
 
 }
 const body = document.body;
@@ -23,6 +25,8 @@ const bookContainer = document.createElement('div');
 bookContainer.classList.add('book-container')
 document.body.append(bookContainer)
 
+// @param takes in an array
+// loops through array and puts each book onto a card
 function displayBooks(library) {
 
     myLibrary.forEach((book, index) => { // loop through the library
@@ -34,8 +38,12 @@ function displayBooks(library) {
         bookCard.append(title);
 
         const author = document.createElement('p');
-        author.textContent = `By ${book.author}`
+        author.textContent = `By ${book.author}`;
         bookCard.append(author);
+
+        const readStatus = document.createElement('p');
+        readStatus.textContent = `Read: ${book.readStatus ? "Yes" : "No"}`;
+        bookCard.append(readStatus);
 
         bookContainer.append(bookCard);
     });
