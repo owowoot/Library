a01ccd6126072b7f38c698279cdee1df2454fcf1
Richard <richardzou25@gmail.com>
2024-11-08 15:59:41 -0500

    displayBook function to add each book to a card for display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const myLibrary = ["Billy Summers"];
 
 class Book {
     constructor(title, author) {
@@ -11,6 +11,27 @@ class Book {
 const billySummers = new Book('Billy Summers', 'Stephen King');
 console.log(billySummers);
 
-function addBookToLibrary(title, author) {
+function addBookToLibrary(book) {
 
 }
+const body = document.body;
+
+function displayBooks(library) {
+    myLibrary.forEach((book, index) => { // loop through the library
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+
+        const title = document.createElement('h3');
+        title.textContent = book.title;
+        bookCard.append(title);
+
+        const author = document.createElement('p');
+        author.textContent = `By ${book.author}`
+        bookCard.append(author);
+
+    });
+}
+
+// Display Books test
+console.log(displayBooks(myLibrary))
+
